5b0178816abfbd3dd204f3b335e26f9f0a99f8dd
Vladimir Meleshchuk <vladimirmeleshuk@gmail.com>
2022-10-18 22:42:43 +0300

    Create and setup base project

---
+++ HW10-GPB-OTUS-MeleshchukVA/Sources/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  HW10-GPB-OTUS-MeleshchukVA
+//
+//  Created by Владимир Мелещук on 17.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
